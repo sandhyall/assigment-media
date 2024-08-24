9ea922db22f80955556e8127a69da21f7068b0b5
sandhya_oic <sandhyadahal864@gmail.com>
2024-08-24 22:55:07 +0545

    added in main

--- react/nextjs/src/app/rockpaper/page.js
+++ react/nextjs/src/app/rockpaper/page.js
@@ -1,43 +1,45 @@
 'use client'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 
 import { FaHandPaper, FaHandRock, FaHandScissors } from "react-icons/fa";
 import Confetti from 'react-confetti'
+import { Button } from '@nextui-org/react';
 
 
 
 const Rockpaper =  () => {
-  const [userScore,setUserScore] = useState(0)
-  const [computerscore,setcomputerScore] = useState(0)
-
   const [userChoice,setUserChoice ] = useState(null)
+  const [newRound, setNewRound] = useState(1)
+const [RandomNum , setRandomNum] = useState(0)
+  
   const handleChange = (choice)=>{
     setUserChoice(choice)
+    setNewRound(newRound+1)
     
     
   }
-  const checkWin = ()=>{
-    
-
-  }
-
-  const choice = [
+const choice = [
     {name:'rock',icon: <FaHandRock size ={100}onClick={()=>handleChange('rock')}/> },
       {name:'paper',icon: <FaHandPaper size ={100}onClick={()=>handleChange('paper')}/> },
       {name:'scissors',icon: <FaHandScissors size ={100}onClick={()=>handleChange('scissors')}/> },
     ]
-  const randomNum = Math.floor(Math.random() * choice.length)
-  const confetti = <Confetti
+    useEffect(()=>{
+      const newNum = Math.floor(Math.random() * choice.length)
+      setRandomNum(newNum)
+  },[userChoice, newRound])
+  const confetti =  <Confetti
   width={1300}
   height={300}
 />
+
   return ( 
     <div>
       <p className='bg-pink-500 flex justify-center items-center shadow-2xl m-4 h-8'>WELCOME TO SCISSOR, PAPER,ROCK GAME</p>
       <div className='bg-blue-500 flex items-center justify-center m-4 p-4 gap-7	'>
       <div className='bg-purple-600 shadow-lg flex h-72 w-[50%] '>computer
-      {userChoice && choice[randomNum].icon}
-      { userChoice && choice[randomNum].name}
+      {userChoice && choice[RandomNum].icon}
+      { userChoice && choice[RandomNum].name}
+     
 
       </div>
       <div className='bg-fuchsia-500 flex h-72 w-[50%] shadow-lg rounded-xl items-center justify-center'>
@@ -56,18 +58,18 @@ const Rockpaper =  () => {
 
       
       </div>
-      { choice[randomNum].name ===  userChoice && "DRAW" } 
-        { choice[randomNum].name === 'paper' &&  userChoice=== 'rock' && "You lost" } 
-        { choice[randomNum].name === 'scissor' &&  userChoice=== 'paper' && "You lost" } 
-        { choice[randomNum].name === 'rock' &&  userChoice=== 'scissor' && "You lost" } 
+      { choice[RandomNum].name ===  userChoice && "DRAW" } 
+        { choice[RandomNum].name === 'paper' &&  userChoice=== 'rock' && "You lost" } 
+        { choice[RandomNum].name === 'scissor' &&  userChoice=== 'paper' && "You lost" } 
+        { choice[RandomNum].name === 'rock' &&  userChoice=== 'scissor' && "You lost" } 
         
-        { choice[randomNum].name === 'rock' &&  userChoice=== 'paper' && confetti} 
-        { choice[randomNum].name === 'paper' &&  userChoice=== 'scissor' && confetti } 
-        { choice[randomNum].name === 'scissor' &&  userChoice=== 'rock' && confetti } 
+        { choice[RandomNum].name === 'rock' &&  userChoice=== 'paper' && confetti} 
+        { choice[RandomNum].name === 'paper' &&  userChoice=== 'scissor' && confetti } 
+        { choice[RandomNum].name === 'scissor' &&  userChoice=== 'rock' && confetti } 
 
-        <div className='bg-orange-400'>your score :</div>
+        <div className='bg-orange-400'>yourscore :</div>
         <div className='bg-orange-400'>computerscore :</div>
-        
+        <Button onClick={()=>setUserChoice(null)} className='flex justify-center items-center bg-blue-400 p-4 shadow-xl'>Reset</Button>
 
       
        </div>
