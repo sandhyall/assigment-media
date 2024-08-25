c4b2127d74e1dd594629bff6eaf57b4ae6629d82
sandhya_oic <sandhyadahal864@gmail.com>
2024-08-25 19:27:43 +0545

    calculator

--- react/nextjs/src/app/calculator/page.js
+++ react/nextjs/src/app/calculator/page.js
@@ -34,6 +34,86 @@
 // export default Calculator
 
 
+// 'use client'
+// import { Button } from '@nextui-org/react'
+// import React, { useState } from 'react'
+
+// const Calculator = () => {
+//     const [output, setOutput] = useState('')
+//     const buttons = [
+//         [{key: 'AC', className: 'bg-gray-300'},
+//         {key: '+/-', className: 'bg-gray-300'},
+//         {key: '%', className: 'bg-gray-300'},
+//         {key: '/', className: 'bg-orange-300'}],
+//         [{key: '7', className: 'bg-gray-300'},
+//         {key: '8', className: 'bg-gray-300'},
+//         {key: '9', className: 'bg-gray-300'},
+//         {key: '*', className: 'bg-orange-300'}],
+//         [{key: '4', className: 'bg-gray-300'},
+//         {key: '5', className: 'bg-gray-300'},
+//         {key: '6', className: 'bg-gray-300'},
+//         {key: '-', className: 'bg-orange-300'}],
+//         [{key: '1', className: 'bg-gray-300'},
+//         {key: '2', className: 'bg-gray-300'},
+//         {key: '3', className: 'bg-gray-300'},
+//         {key: '+', className: 'bg-orange-300'}],
+//         [{key: '0', className: 'bg-gray-300'},
+//         {key: '.', className: 'bg-gray-300'},
+//         {key: '=', className: 'bg-orange-300'}]
+//     ]
+
+//     const handleChange = (key)=>{
+//       switch(key){
+//         case '+', '-','*','/':
+//             setOutput(output+ key)
+//             break;
+//         case 'AC':
+//             setOutput('')
+//             break;
+
+//         case '+/-':
+//             setOutput(eval(output*-1))
+//             break;
+
+//             case '=':
+//             setOutput(eval(output))
+//             break;
+
+//             case '%':
+//             setOutput(output/100)
+//             break;
+
+//             case '->':
+//             setOutput(output.toString().slice(0,-1))
+//             break;
+
+
+            
+//         default:
+//             setOutput(output+ key)
+            
+
+//             break;
+//       }
+
+//     }
+//   return (
+//     <div className='bg-black p-2 text-white'>
+//         <div className='text-white'>{output}</div>
+//         {buttons.map((item,id)=>{
+//             return <div className='m-4'>
+//                 {item.map((val,idx)=>{
+//                   return  <Button onClick={()=> handleChange(val.key)} className={val.className + ' rounded-full mx-1'}>{val.key}</Button>
+//                 })}
+//             </div>
+//         })}
+//     </div>
+//   )
+// }
+
+// export default Calculator 
+
+
 'use client'
 import { Button } from '@nextui-org/react'
 import React, { useState } from 'react'
@@ -41,63 +121,65 @@ import React, { useState } from 'react'
 const Calculator = () => {
     const [output, setOutput] = useState('')
     const buttons = [
-        [{key: 'AC', className: 'bg-gray-300'},
-        {key: '+/-', className: 'bg-gray-300'},
-        {key: '%', className: 'bg-gray-300'},
-        {key: '/', className: 'bg-orange-300'}],
-        [{key: '7', className: 'bg-gray-300'},
-        {key: '8', className: 'bg-gray-300'},
-        {key: '9', className: 'bg-gray-300'},
+        [{key: 'AC', className:'mb-1 rounded   bg-gray-500  text-white decoration-1'},
+        {key: '+/-', className: 'mb-1 rounded   bg-gray-500  text-white decoration-1'},
+        {key: '%', className: 'mb-1 rounded   bg-gray-500  text-white decoration-1'},
+        {key: '/', className: 'mb-1 rounded  bg-gray-500  text-white decoration-1'}],
+        [{key: '7', className: 'mb-1 rounded   bg-gray-500  text-white decoration-1'},
+        {key: '8', className: 'mb-1 rounded   bg-gray-500  text-white decoration-1'},
+        {key: '9', className: 'mb-1 rounded   bg-gray-500  text-white decoration-1'},
         {key: '*', className: 'bg-orange-300'}],
-        [{key: '4', className: 'bg-gray-300'},
-        {key: '5', className: 'bg-gray-300'},
-        {key: '6', className: 'bg-gray-300'},
+        [{key: '4', className: 'mb-1 rounded   bg-gray-500  text-white decoration-1'},
+        {key: '5', className: 'mb-1 rounded   bg-gray-500  text-white decoration-1'},
+        {key: '6', className: 'mb-1 rounded  bg-gray-500  text-white decoration-1'},
         {key: '-', className: 'bg-orange-300'}],
-        [{key: '1', className: 'bg-gray-300'},
-        {key: '2', className: 'bg-gray-300'},
-        {key: '3', className: 'bg-gray-300'},
+        [{key: '1', className: 'mb-1 rounded   bg-gray-500  text-white decoration-1'},
+        {key: '2', className: 'mb-1 rounded  bg-gray-500  text-white decoration-1'},
+        {key: '3', className: 'mb-1 rounded  bg-gray-500  text-white decoration-1'},
         {key: '+', className: 'bg-orange-300'}],
-        [{key: '0', className: 'bg-gray-300'},
-        {key: '.', className: 'bg-gray-300'},
-        {key: '=', className: 'bg-orange-300'}]
+        [{key: '0', className: 'mb-1 rounded   bg-gray-500  text-white decoration-1'},
+        {key: '.', className: 'mb-1 rounded  bg-gray-500  text-white decoration-1'},
+        {key: '<-', className: 'bg-orange-300'},
+        {key: '=', className: 'bg-orange-300 row-end-5'}
+      ]
     ]
+    const arthimeticKeys = ['/','*']
 
     const handleChange = (key)=>{
+        if(arthimeticKeys.includes(output.toString().slice(-1)) && arthimeticKeys.includes(key)){
+        const result =  output.slice(0,output.length-1)+ key
+        setOutput(result)
+        return;
+        }else if(output.toString().slice(-1) == key && isNaN(Number(key)) ){
+          return;
+        }
       switch(key){
         case '+', '-','*','/':
-            setOutput(output+ key)
+            setOutput(output.toString()+ key)
             break;
         case 'AC':
             setOutput('')
             break;
-
+        case '=':
+          setOutput(eval(output))
+          break;
         case '+/-':
             setOutput(eval(output*-1))
             break;
-
-            case '=':
-            setOutput(eval(output))
-            break;
-
-            case '%':
-            setOutput(output/100)
-            break;
-
-            case '->':
-            setOutput(output.toString().slice(0,-1))
-            break;
-
-
-            
+        case '%':
+          setOutput(eval(output/100))
+          break;
+        case '<-':
+          setOutput(output.toString().slice(0,-1))
+          break;
         default:
             setOutput(output+ key)
-            
-
             break;
       }
 
     }
   return (
+    
     <div className='bg-black p-2 text-white'>
         <div className='text-white'>{output}</div>
         {buttons.map((item,id)=>{
@@ -108,7 +190,12 @@ const Calculator = () => {
             </div>
         })}
     </div>
+    
+    
   )
 }
 
-export default Calculator  
+export default Calculator
+
+
+            
