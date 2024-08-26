129fcfaea1fef9067532fd624f2d832b0a70eb64
sandhya_oic <sandhyadahal864@gmail.com>
2024-08-26 07:14:03 +0545

    counter

--- react/nextjs/src/app/facebook/page.js
+++ react/nextjs/src/app/facebook/page.js
@@ -1,19 +1,45 @@
+
 'use client'
 import { Button } from '@nextui-org/react';
 import React, { useState } from 'react'
 import { GoThumbsup } from "react-icons/go";
+import { FaRegAngry } from "react-icons/fa";
+import { FaSurprise } from "react-icons/fa";
+import { FaLaughSquint } from "react-icons/fa";
+const Facebook = () => {
+    const [reaction, setReaction] = useState(null)
+    const [openDiv, setOpenDiv] = useState(false)
+    const changeReaction = (newReaction) => {
+        setReaction(reaction === newReaction ? null : newReaction)
+    }   
 
-const facebook = () => {
-  const [reaction, setReaction] = useState(null)
-const changeReaction = (newReaction) =>{
-  setReaction(newReaction)
-}
-  return (
-    <div>
-     <Button className = {reaction=='like'? 'bg-blue-400': null}onClick={()=>changeReaction('like')}> <GoThumbsup /> Like </Button> 
-     <Button className = {reaction=='like'? 'bg-blue-400': null}onClick={()=>changeReaction('like')}> <GoThumbsup /> WoW </Button> 
-    </div>
-  )
+
+    const GenerateReactionButton = () => {
+        if(reaction == 'angry'){
+            return <Button onMouseEnter={()=> setOpenDiv(true)} onClick={()=>changeReaction('angry')}><FaRegAngry color='red'/> Angry</Button>
+        }else if(reaction == 'wow'){
+            return <Button   onMouseEnter={()=> setOpenDiv(true)} onClick={()=>changeReaction('wow')}>  <FaSurprise color='orange'/> {reaction} </Button>
+        }else if(reaction == 'haha'){
+            return <Button onMouseEnter={()=> setOpenDiv(true)} onClick={()=>changeReaction('haha')}>  <FaSurprise  color='orange'/> {reaction} </Button>
+        }else {
+            return <Button  onMouseEnter={()=> setOpenDiv(true)} className={reaction=='like'? 'text-blue-700 font-semibold': null}  onClick={()=>changeReaction('wow')}>  <GoThumbsup /> {reaction || 'Like'} </Button>
+        }
+    }
+    return (
+        <div>
+            {openDiv && (
+                <div onMouseLeave={()=> setTimeout(() => {
+                    setOpenDiv(false)
+                }, 1000)} className='flex gap-4 m-2 p-2 shadow-md w-36'>
+                <GoThumbsup size={50} onClick={()=>changeReaction('like')}/> 
+                <FaRegAngry size={50} color='red' onClick={()=>changeReaction('angry')}/> 
+                <FaSurprise size={50} color="orange" onClick={()=>changeReaction('wow')}/> 
+                <FaLaughSquint size={50} color="orange" onClick={()=>changeReaction('haha')}/> 
+                </div>
+            )}
+            <GenerateReactionButton/>
+        </div>
+      )
 }
 
-export default facebook
+export default Facebook
--- react/nextjs/src/redux/store.js
+++ react/nextjs/src/redux/store.js
@@ -1,5 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { counterSlice } from './reducerslice/counterslice'
 
 export default configureStore({
-  reducer: {}
+  reducer: {
+    counter: counterSlice
+  },
 })
