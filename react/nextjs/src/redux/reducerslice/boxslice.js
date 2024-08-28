8134b9355c221fef1bcaffa27a1ebf39506aefe2
sandhya_oic <sandhyadahal864@gmail.com>
2024-08-28 07:46:43 +0545

    box

--- react/nextjs/src/redux/reducerslice/boxslice.js
+++ react/nextjs/src/redux/reducerslice/boxslice.js
@@ -5,24 +5,40 @@ export const boxSlice = createSlice({
   initialState: {
     width: 40,
     height: 40,
-    backgroundColor: 'red',
+    backgroundColor: 'blue',
     borderRadius: 0,
     padding:20,
     margin:20
+    
+
   },
   reducers: {
     changeToCircle: (state, action) => {
-       state.height=state.width
-       state.borderRadius= '50%'
+      state.height=state.width
+state.borderRadius= '50%'
     },
-    increaseWidth: (state, action) => {
+ 
+increaseWidth: (state, action) => {
       state.width++
    },
+   changeTorectangle: (state, action) => {
+    state.height=state.width
+    state.borderRadius= '0%'
+ },
+    changebackgroundColor:(state,action)=>{
+      state.backgroundColor=action.payload
+    },
+    moveRight: (state, action) => {
+      state.marginLeft= state.marginLeft+20
+    },
+    moveLeft: (state, action) => {
+      state.marginRight= state.marginRight+20
+    },
     
   }
 })
 
 
-export const { changeToCircle,increaseWidth } = boxSlice.actions
+export const { changeToCircle,increaseWidth,changeTorectangle,changebackgroundColor,moveRight,moveLeft } = boxSlice.actions
 
 export default boxSlice.reducer
